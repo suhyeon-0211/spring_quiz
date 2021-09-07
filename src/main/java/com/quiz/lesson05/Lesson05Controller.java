8997c3ef0d5fa4f2bccdc5872782637f02df249c
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2021-09-07 20:36:05 +0900

    lesson05 quiz03 파일 추가

--- src/main/java/com/quiz/lesson05/Lesson05Controller.java
+++ src/main/java/com/quiz/lesson05/Lesson05Controller.java
@@ -69,4 +69,38 @@ public class Lesson05Controller {
 		
 		return "lesson05/quiz02";
 	}
+	
+	@RequestMapping("/quiz03")
+	public String quiz03(Model model) {
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057);
+		model.addAttribute("candidates", candidates);
+		
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		model.addAttribute("cardBills", cardBills);
+		return "lesson05/quiz03";
+	}
 }
